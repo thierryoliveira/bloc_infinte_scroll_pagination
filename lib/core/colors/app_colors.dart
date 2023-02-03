1ad971f042de770d6334dc9051463d454e39541e
Thierry Oliveira <thierry.ifsp@gmail.com>
2023-02-03 00:00:07 -0300

    feat: create app colors class

---
+++ lib/core/colors/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color black = Color(0xff131318);
+  static const Color grey = Color(0xff1C1F27);
+  static const Color purple = Color(0xff5265F5);
+  static const Color green = Color(0xff04A777);
+  static const Color red = Color(0xffF2545B);
+  static const Color white = Color(0xffF3F3F3);
+}
