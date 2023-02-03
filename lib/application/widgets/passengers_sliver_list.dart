3df82c73bb71d230c0bcdfc321dce10f19e063d7
Thierry Oliveira <thierry.ifsp@gmail.com>
2023-02-03 00:00:56 -0300

    feat: improve passenger list item UI using custom ticket shape

--- lib/application/widgets/passengers_sliver_list.dart
+++ lib/application/widgets/passengers_sliver_list.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:infinite_scroll_bloc/core/colors/app_colors.dart';
+import 'package:infinite_scroll_bloc/core/widgets/custom_ticket_shape.dart';
 import 'package:infinite_scroll_bloc/domain/entities/passanger.entity.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
@@ -8,16 +11,94 @@ class PassengersSliverList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PagedSliverList<int, PassengerEntity>(
-        pagingController: pagingController,
-        builderDelegate: PagedChildBuilderDelegate<PassengerEntity>(
-          itemBuilder: (context, item, index) {
-            return ListTile(
-              leading: Text(index.toString()),
-              title: Text(item.name ?? 'no name'),
-              trailing: Text(item.trips?.toString() ?? 'no trips'),
-            );
-          },
-        ));
+    final size = MediaQuery.of(context).size;
+
+    return PagedSliverList<int, PassengerEntity>.separated(
+      pagingController: pagingController,
+      builderDelegate: PagedChildBuilderDelegate<PassengerEntity>(
+        itemBuilder: (context, item, index) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ClipPath(
+                clipper: CustomTicketShape(),
+                child: Container(
+                  constraints: BoxConstraints(
+                      minWidth: size.width * .9, minHeight: size.height * .1),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 36),
+                  color: AppColors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            item.name ?? 'no name',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 20),
+                          ),
+                          Text(
+                              ('${item.airline?.last.name} - ${item.airline?.last.country}')),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16),
+                            child: SizedBox(
+                              height: size.height * .08,
+                              child: VerticalDivider(
+                                thickness: 2,
+                                color: AppColors.grey.withOpacity(.26),
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                FontAwesomeIcons.plane,
+                                color: AppColors.purple,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 4),
+                                child:
+                                    Text(item.trips?.toString() ?? 'No trips'),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+                // child: ListTile(
+                //   contentPadding: const EdgeInsets.all(16),
+                //   tileColor: AppColors.white,
+                //   title: Text(
+                //     item.name ?? 'no name',
+                //     style: const TextStyle(
+                //         fontWeight: FontWeight.w600, fontSize: 20),
+                //   ),
+                //   subtitle: Text(
+                //       ('${item.airline?.last.name} - ${item.airline?.last.country}')),
+                //   trailing: Column(
+                //     mainAxisAlignment: MainAxisAlignment.center,
+                //     children: [
+                //       const Icon(FontAwesomeIcons.plane),
+                //       Text(item.trips?.toString() ?? 'No trips'),
+                //     ],
+                //   ),
+                // ),
+                ),
+          );
+        },
+      ),
+      separatorBuilder: (context, index) => const SizedBox(height: 16),
+    );
   }
 }
